fc00e9e7206253f1de48a11f35e8041f64c5656b
marsxxx100 <marsxxx100@gmail.com>
2022-06-26 16:00:38 +0500

    Второй коммит

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("ВВЕДИТЕ ЧИСЛО А - ");
+int a = Int32.Parse(Console.ReadLine());
+Console.Write("ВВЕДИТЕ ЧИСЛО Б - ");
+int b = Int32.Parse(Console.ReadLine());
+
+int c = a - b;
+int d = a + b;
+int e = a * b;
+float f1 = a / b;
+double f = (double)a / (double)b;
+int g = a % b;
+
+Console.WriteLine("ВАШ РЕЗУЛЬТАТ");
+
+Console.WriteLine("Разность - " + c.ToString());
+Console.WriteLine("СУММА - " + d.ToString());
+Console.WriteLine("ПРОИЗВЕДЕНИЕ - " + e.ToString());
+Console.WriteLine("Деление - " + f.ToString());
+Console.WriteLine("Деление_1 - " + f1.ToString());
+Console.WriteLine("Остаток - " + g.ToString());
